6df5c41e77b0237acc386e42fcbbd4b759143077
Leonard514 <Leonard514@users.noreply.github.com>
2022-11-16 22:30:59 +0000

    deploy: b5c9148f3af76be8ce188868e6f4aaf1686b5718

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/APCSP-T2/assets/js/search-data.json', true);
+    request.open('GET', '/APCSPT2/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
